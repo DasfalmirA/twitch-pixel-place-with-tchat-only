4c28dca910c36e11c066904200d4c979ed4cb29e
nicolas-denys <55084230+nicolas-denys@users.noreply.github.com>
2023-03-20 13:18:20 +0100

    First Update
    Program that :
     - create a window with pygame.
     - put a pixel with twitchio (remember to use your own token on myAuthTwitch.py)
     - censor with a right click on the screen
     - load and save your image
     - do a bip music depending on the intensity of the pixel
    - use asyncio to get 60 fps more or less

---
+++ myAuthTwitch.py
@@ -0,0 +1,5 @@
+# Place ton token entre les guillemets
+# Si vous n'avez pas de token, vous pouvez utiliser https://twitchtokengenerator.com/ ou aller dans https://dev.twitch.tv/console et récupérer un token
+token=""
+#Place le nom de ta chaîne entre apostrophes
+name = ''
